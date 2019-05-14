59f05420d9a2d5fa6e22f12bdf6611b623c5d74e
manasa-gavini <manasagavini.d17@iiits.in>
2019-05-14 15:09:14 +0530

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,190 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return new MaterialApp(
+      title: 'Pet Realm',
+      theme: new ThemeData(
+        primarySwatch: Colors.yellow,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyHomePage extends StatelessWidget {
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+    Widget build(BuildContext context) {
+     
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Pet Realm"),
+          centerTitle: true,
+        ),
+        body: 
+           CircleImages()
+          
+      );
+    }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-   final stories = Expanded(
-    child: new Padding(
-      padding: const EdgeInsets.only(top: 8.0,bottom: 420.0),
+// class MyHomePage extends StatelessWidget {
+
+//   @override
+//     Widget build(BuildContext context) {
+//      
+//       return Scaffold(
+//         appBar: AppBar(
+//           title: Text("circle"),
+//         ),
+//         body: Center(
+//           child: Container(
+                  
+//                   height: 80.0,
+//                   width: 80.0,
+//                   margin: EdgeInsets.only(
+//                     left: 5.0,
+//                     right: 5.0
+//                   ),
+//                   decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(75.0),
+//                     border: Border.all(
+//                       width: 2.0,
+//                       style:BorderStyle.solid ,
+//                       color: Color.fromARGB(255, 0 , 0, 0)
+//                     ),
+//                     image: DecorationImage(
+//                       fit: BoxFit.cover,
+//                       image: NetworkImage("https://cdn.dribbble.com/users/1368/screenshots/1785863/icons_2x.png")
+//                     )
+//                   )
+//                 ) 
+//         ) ,
+//       );
+//     }
+// }
+
+class CircleImages extends StatefulWidget{
+  
+@override
+  State<StatefulWidget> createState() {
     
-      child: new Container(
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-        new ListView(
-        
-        scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                new Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new AssetImage("assets/dog2.jpg"),
-                            ),
-                  ),
-                
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                ),
-                new Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new AssetImage("assets/cat.jpg"),
-                            ),
-                  ),
-                
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                ),
-                new Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new AssetImage("assets/birds.jpg"),
-                            ),
-                  ),
-                
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                ),
-                new Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new AssetImage("assets/fish.jpg"),
-                            ),
-                  ),
-                
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                ),
-                new Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new AssetImage("assets/horse.jpg"),
-                            ),
-                  ),
-                
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                ),
-                
-              ],
-       
-   
-        ), 
-
-          ],
-    ),),
-  ),);
-
-   @override
-  Widget build(BuildContext context) {
-    return new Container(
-      margin: const EdgeInsets.all(16.0),
-      child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          stories,
-        ],),);
-
+    return CircleWidgets();
   }
 }
 
-
+class CircleWidgets extends State<CircleImages>{
+  @override
+    Widget build(BuildContext context) {
+      List<Widget> widgets = [];
+      //for(var x = 0 ; x < 10 ; x++){
+        widgets.add(Container(
+                  height: 75.0,
+                  width: 125.0,
+                  margin: EdgeInsets.all(
+                    20.0
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(75.0),
+                    boxShadow:[
+                      new BoxShadow(
+                        color: Color.fromARGB(100, 0, 0, 0),
+                        blurRadius: 5.0,
+                        offset: Offset(5.0, 5.0)
+                      )
+                    ], 
+                    
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: new AssetImage("assets/dog2.jpg"),
+                    )
+                  )
+                ) 
+            );
+      widgets.add(Container(
+                  height: 75.0,
+                  width: 125.0,
+                  margin: EdgeInsets.all(
+                    20.0
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(75.0),
+                    boxShadow:[
+                      new BoxShadow(
+                        color: Color.fromARGB(100, 0, 0, 0),
+                        blurRadius: 5.0,
+                        offset: Offset(5.0, 5.0)
+                      )
+                    ], 
+                   
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: new AssetImage("assets/cat.jpg"),
+                    )
+                  )
+                ) 
+            );
+            widgets.add(Container(
+                  height: 75.0,
+                  width: 125.0,
+                  margin: EdgeInsets.all(
+                    20.0
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(75.0),
+                    boxShadow:[
+                      new BoxShadow(
+                        color: Color.fromARGB(100, 0, 0, 0),
+                        blurRadius: 5.0,
+                        offset: Offset(5.0, 5.0)
+                      )
+                    ], 
+                   
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: new AssetImage("assets/birds.jpg"),
+                    )
+                  )
+                ) 
+            );
+            widgets.add(Container(
+                  height: 75.0,
+                  width: 125.0,
+                  margin: EdgeInsets.all(
+                    20.0
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(75.0),
+                    boxShadow:[
+                      new BoxShadow(
+                        color: Color.fromARGB(100, 0, 0, 0),
+                        blurRadius: 5.0,
+                        offset: Offset(5.0, 5.0)
+                      )
+                    ], 
+                    
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: new AssetImage("assets/fish.jpg"),
+      
+                    )
+                  )
+                 // child: Text("Fish"),
+                ) 
+            );
+      return Container(
+        height: 175.0,
+        
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.all(5.0),
+          children: widgets
+        )
+      );
+      
+    }
+}
